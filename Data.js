f590b9793e886228165c106993458fb667d64983
Tejasri10 <144254822+Tejasri10@users.noreply.github.com>
2024-02-13 16:56:26 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,71 @@
+import headset from './assests/headset.jpg'
+import ipad from './assests/ipad.jpg'
+import laptop from './assests/laptop.jpg'
+import mobile from './assests/mobile.jpg'
+import keybord from './assests/keyboard.jpg' 
+import birds from './assests/birds.jpg'
+import securitycam from './assests/securitycam.jpg'
+import drone from './assests/drone.jpg'
+
+
+const data = {
+    productItems:[
+        {
+            id: "1",
+            name: "headset",
+            cost: 1200,
+            pic: headset
+        
+        },
+        {
+            id: "2",
+            name:"ipad",
+            cost: 5000,
+            pic: ipad
+        },
+        {
+            id: "3",
+            name: "laptop",
+            cost: 40000,
+            pic: laptop
+
+        },
+        {
+            id: "4",
+            name:"mobile",
+            cost:9000,
+            pic: mobile
+        },
+        
+        {
+            id: "5",
+            name:"keybord",
+            cost:2500,
+            pic: keybord
+        },
+        {
+            id: "6",
+            name:"birds",
+            cost:500,
+            pic: birds
+        },
+        {
+            id: "7",
+            name:"securitycam",
+            cost:8000,
+            pic: securitycam
+        },
+        {
+            id: "8",
+            name:"drone",
+            cost:80000,
+            pic: drone
+        },
+        
+        
+        
+    ]
+
+
+}
+export default data
